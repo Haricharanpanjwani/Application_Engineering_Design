90988b1974ad2db59c410f091c67c611e800c0bd
Hari Panjwani <panjwani.h@husky.neu.edu>
2016-05-01 22:58:28 -0400

    Commiting Final Project

---
+++ Final Project - Pain Management iOS/PainManagement-Server/src/main/java/com/my/finalproject/editprofile/EditPatientProfileModel.java
@@ -0,0 +1,77 @@
+package com.my.finalproject.editprofile;
+
+public class EditPatientProfileModel {
+	
+	private int userid;
+	private String firstname;
+	private String lastname;
+	private String gender;
+	private String language;
+	private String enthnonym;
+	private String enthnicity;
+	private String currentlyWorking;
+	private String startdate;
+	private int weight;
+	
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getEnthnicity() {
+		return enthnicity;
+	}
+	public void setEnthnicity(String enthnicity) {
+		this.enthnicity = enthnicity;
+	}
+	public String getCurrentlyWorking() {
+		return currentlyWorking;
+	}
+	public void setCurrentlyWorking(String currentlyWorking) {
+		this.currentlyWorking = currentlyWorking;
+	}
+	public String getStartdate() {
+		return startdate;
+	}
+	public void setStartdate(String startdate) {
+		this.startdate = startdate;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getEnthnonym() {
+		return enthnonym;
+	}
+	public void setEnthnonym(String enthnonym) {
+		this.enthnonym = enthnonym;
+	}
+	
+}
